9d2d5c30ada5c8dec2c60600b69a0f5c9a56a81b
FaridLesther <61443299+FaridLesther@users.noreply.github.com>
2021-04-12 23:53:28 -0600

    Elegir imagen de perfil de usuario

--- WQuick/app/usuarios/admin.py
+++ WQuick/app/usuarios/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from usuarios import models
 # Register your models here.
+admin.site.register(models.Usuario)
+admin.site.register(models.Proyecto)
+admin.site.register(models.Freelancer)
